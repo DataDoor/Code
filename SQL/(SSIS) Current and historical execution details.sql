0fa93b4dd6a1f9c92951ab8cbd01efb8098ba25f
Neil Gelder <ngelder@emeraldgroup.com>
2018-01-25 08:56:35 +0000

    altered back to normal

--- SQL/(SSIS) Current and historical execution details.sql
+++ SQL/(SSIS) Current and historical execution details.sql
@@ -1,23 +1,28 @@
 /*SSIS CATALOG QUERIES*/
 
-SELECT 
-	event_message_id
-	,[Message]
-	,package_name
-	,event_name
-	,message_source_name
-	,package_path
-	,execution_path
-	,message_type
-	,message_source_type
-FROM SSISDB.CATALOG.event_messages em(NOLOCK) 
-INNER JOIN (SELECT MAX(execution_id)  AS execution_id, folder_name FROM SSISDB.catalog.executions GROUP BY folder_name) e
-	ON em.operation_id = e.execution_id
-/* Frequently used predicates*/
---WHERE folder_name = <Folder Name,1,'FolderName'> AND project_name = <Project Name,1,'Projectname'>
---WHERE	event_name = 'OnError'
---WHERE	package_name = 'ScopusCitation.dtsx'
+SELECT
+event_message_id
+,[Message]
+,package_name
+,event_name
+,message_source_name
+,package_path
+,execution_path
+,message_type
+,message_source_type
+FROM (
+SELECT em.*
+FROM SSISDB.CATALOG.event_messages em(NOLOCK)
+WHERE em.operation_id = (SELECT MAX(execution_id) FROM SSISDB.catalog.executions) --Find currently executing details
+--WHERE em.operation_id = (SELECT MAX(execution_id) FROM SSISDB.CATALOG.executions WHERE folder_name = 'SSIS Catalog folder name') --Find last execution at folder level
+--WHERE em.operation_id = (SELECT MAX(execution_id) FROM SSISDB.CATALOG.executions WHERE folder_name = 'SSIS Catalog folder name' AND project_name = 'Project name') --Find last execution at project level
+) q
+/* Put in whatever WHERE predicates you might like*/
+--WHERE event_name NOT LIKE '%Validate%'
+--WHERE event_name = 'OnError'
+--WHERE package_name = 'packagename.dtsx'
 --WHERE execution_path LIKE '%<some executable>%'
+ORDER BY message_time DESC
 
 
 
