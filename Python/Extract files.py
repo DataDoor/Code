cecb2eb6b5272a8909ae26d21f8fea723fe27037
EGP <egpdata@outlook.com>
2018-04-23 14:20:34 +0100

    changes to be more dynamic for extract

--- Python/Extract files.py
+++ Python/Extract files.py
@@ -1,15 +1,44 @@
 import zipfile, os, shutil
 
-filePath = '\\\\DT014\\AcquiredContent\\WJE\\Unzipped'
-xmlFileStorePath = '\\\\DT014\\AcquiredContent\\XmlFiles\\WJE'
+# ##One level down
 
+# filePath = '\\\\LP324\\DataShare\\ContentExtract\\Zipped'
+# upzipPath = '\\\\LP324\\DataShare\\ContentExtract\\UnZipped'
+
+
+
+# ##Extract Zip Contents
+
+# print('EXTRACT FILES FROM ZIP')
+
+# for zipFileName in os.listdir(filePath):
+
+#     zipFileExtract = zipfile.ZipFile(f'{filePath}\\{zipFileName}')
+#     zipFileExtract.extractall(upzipPath)
+#     zipFileExtract.close()
+
+# print('EXTRACT FROM ZIP COMPLETED')
+
+#Two Level Down
+
+filePath = '\\\\LP324\\DataShare\\ContentExtract\\UnZipped'
+xmlStorePath = '\\\\LP324\\DataShare\\ContentExtract\\XML'
 
 print('MOVE XML TO XML FILE STORE')
 for lvlOneFolder in os.listdir(filePath):
-    lvlOnePath = filePath+'\\'+lvlOneFolder
+    lvlOnePath = f'{filePath}\\{lvlOneFolder}'
     for lvlTwoFolder in os.listdir(lvlOnePath):
-        lvlTwoPath = filePath+'\\'+lvlOneFolder+'\\'+lvlTwoFolder
+        lvlTwoPath = f'{filePath}\\{lvlOneFolder}\\{lvlTwoFolder}'
         for fileName in os.listdir(lvlTwoPath):
             if fileName.endswith('.xml'):
-                shutil.move(lvlTwoPath+'\\'+fileName,xmlFileStorePath+'\\'+fileName)
+                currentPath = f'{lvlTwoPath}\\{fileName}'
+                movePath = f'{xmlStorePath}\\{fileName}'
+
+            try:
+                shutil.copy(currentPath,movePath)
+                #print (f'{fileName} - Copied')
+            except:
+                print(f'Move failed for {fileName}')
+                continue
+                
 print('XML FILE MOVE COMPLETED')
