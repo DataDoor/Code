5e557c9b788d8171ce71aeeac026aa0cbfeb3660
EGPData <EGPData@outlook.com>
2018-11-29 13:03:24 +0000

    Changes boom

--- Python/API.py
+++ Python/API.py
@@ -1,57 +1,25 @@
-
 ##IMPORT REQURIED PYTHON LIBRARIES  
 
 import requests
 from requests.auth import HTTPBasicAuth 
-import pyodbc
 
-##CREATE CONNECTION TO SQL SERVER DATABASE
 
-conn = pyodbc.connect(
-    r'DRIVER={ODBC Driver 13 for SQL Server};'
-    r'SERVER=localhost;'
-    r'DATABASE=DB;'
-    r'Trusted_Connection=yes;'
 
-    ) 
 
-##SET VARIABLES
 
-url = ''
+"""
+SET API VARIABLES
+"""
+
+##SET Environment
+
+#URL API
 apiuser = ''
 apipass = ''
+url = ''
 
 
-##CALL GET API
-
-print('Call GET API - {0}'.format(url))
-r = requests.get(url, auth=HTTPBasicAuth(apiuser, apipass))
-
-#REPLACE UTF-8 WITH UTF-16, THIS IS REQUIRED AS SQL SERVER ONLY ACCEPTS UTF-16
-xmlString = str(r.text).replace("UTF-8","UTF-16")
-
-#OPEN DATABASE CONNECTION
-print('Opening dbConnection')
-cursor = conn.cursor() 
+r = requests.get(url,auth=HTTPBasicAuth(apiuser, apipass))
 
 
-#CREATE SQL STATEMENT FOR INSERTING XML INTO TABLE
-SQLCommand = ("INSERT INTO APIXMLTest(XMLData) VALUES (?)")
-Values = [xmlString]
-            
-
-#PROCESSING QUERY    
-cursor.execute(SQLCommand,Values)  
-
-#COMMIT TRANSACTION TO DATABASE 
-conn.commit() 
-
-
-#CLOSE DATABASE CONNECTION.
-print('Close dbConnection')
-conn.close()   
-
-
-
-
-
+print(r.status_code)
--- SQL/(SSIS) Current and historical execution details.sql
+++ SQL/(SSIS) Current and historical execution details.sql
@@ -14,7 +14,7 @@ FROM (
 SELECT em.*
 FROM SSISDB.CATALOG.event_messages em(NOLOCK)
 --WHERE em.operation_id = (SELECT MAX(execution_id) FROM SSISDB.catalog.executions) --Find currently executing details
-WHERE em.operation_id = (SELECT MAX(execution_id) FROM SSISDB.CATALOG.executions WHERE folder_name = 'GDPR') --Find last execution at folder level
+WHERE em.operation_id = (SELECT MAX(execution_id) FROM SSISDB.CATALOG.executions WHERE folder_name = 'PTS_CRM') --Find last execution at folder level
 --WHERE em.operation_id = (SELECT MAX(execution_id) FROM SSISDB.CATALOG.executions WHERE folder_name = 'SSIS Catalog folder name' AND project_name = 'Project name') --Find last execution at project level
 ) q
 /* Put in whatever WHERE predicates you might like*/
